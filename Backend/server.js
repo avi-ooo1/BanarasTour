80295b1faac29432e46266dac2ebd6beeab90fbd
Your Name <your.email@example.com>
2026-04-08 17:08:34 +0530

    Update Backend server.js

--- Backend/server.js
+++ Backend/server.js
@@ -48,7 +48,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Handle preflight requests
+app.options(/^(.*)$/, cors(corsOptions));
 
 
 
